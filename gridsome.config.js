f5ae94b3200946047a7e130a86112b2122cfba86
Alex Cresswell <ukajc1@googlemail.com>
2025-03-29 21:08:51 +0000

    Refactored and added github pages deployment config

---
+++ gridsome.config.js
@@ -0,0 +1,11 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Y4K',
+  siteUrl: 'https://y4k.github.io/',
+  plugins: []
+}
