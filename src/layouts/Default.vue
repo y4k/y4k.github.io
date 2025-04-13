f998a47c9ccc55338f1f853f63f9f86795683ca9
Alex Cresswell <ukajc1@googlemail.com>
2025-04-13 15:31:18 +0100

    Simple changes to add username and password inputs

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -9,7 +9,7 @@
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -23,15 +23,12 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   line-height: 1.5;
 }
 
 .layout {
   max-width: 760px;
-  margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,36 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div class="horizontal-container">
+      <div class="vertical-container" style="margin-right: 1em;">
+        <p>Username</p>
+        <p>Password</p>
+      </div>
+      <div class="vertical-container">
+        <input type="text" id="username" name="username" required minlength="1" maxlength="20" />
+        <input type="text" id="password" name="password" required minlength="1" maxlength="20" />
+      </div>
+    </div>
+    <button>Register</button>
   </Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+<script setup>
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+p {
+  margin: 0
+}
+
+.vertical-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.horizontal-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 </style>
